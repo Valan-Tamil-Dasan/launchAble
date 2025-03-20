13d974f0e48e35191ef5cfbe50fa293d753e1807
Valan <valantamildasan@gmail.com>
2025-03-20 10:13:12 +0530

    Login, SignUp

---
+++ src/app/login/page.tsx
@@ -0,0 +1,14 @@
+import { LoginForm } from "@/components/login-form";
+
+function LoginPage(){
+
+  return(
+    <>
+      <div className="flex bg-gray-100 items-center h-full justify-center mt-auto">
+        <LoginForm/>
+      </div>
+    </>
+  )
+};
+
+export default LoginPage;
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,15 @@
+import { SignupForm } from "@/components/signup-form";
+
+function SignupPage(){
+
+  return(
+    <>
+      <div className="flex bg-gray-100 items-center h-full justify-center mt-auto">
+        <SignupForm/>
+      </div>
+    </>
+  )
+};
+
+export default SignupPage;
+
